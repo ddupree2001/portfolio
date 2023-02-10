dc240cf03dd98e0f54469f2b1b240f19d9cb934e
DeVon DuPree <devondupree@Air-DeVonny-Pooh.local>
2023-02-09 17:20:31 -0800

    about

---
+++ portfolio/src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import me from '../assets/me.png'
+
+const About = () => {
+  return (
+    <div className="flex flex-col-reverse md:flex-row justify-between items-center mt-[100px] gap-10 mx-[50px]">
+        <div className="w-1/2 md:w-1/3">
+            <img src={me} alt="" className=""/>
+        </div>
+        <div className="w-full md:w-2/3">
+            <h1 className="font-bold text-[1.5rem] sm:text-[2.25rem]">About Me</h1>
+            <p className="text-[1.125rem] sm:text-[1.5rem] mt-[20px]">I am a full-stack web developer with a passion for creating amazing user experiences and bringing innovative ideas to life. Equipped with skills in multiple programming languages and a deep understanding of both front-end and back-end development, I am ready to tackle any web development challenge. I am experienced in agile methodologies and always seeking new opportunities to expand my skillset. I am eager to work with a dynamic and forward-thinking company where I can continue to grow and make a meaningful impact through my work. As a passionate developer, I am committed to bringing my expertise and attention to detail to every project I work on.</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,7 +1,9 @@
 import Navbar from './Navbar';
 import Header from './Header';
+import About from './About';
 
 export {
     Navbar,
     Header,
+    About,
 }
